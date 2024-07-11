2eda557deb8c3b46a1c8e53f306026968d89dc1b
Yanina Lysukha <yanina.lysukha@gmail.com>
2024-07-11 12:35:01 +0200

    feat: add a functional ListItem component

--- class-components/src/components/ListItem/ListItem.tsx
+++ class-components/src/components/ListItem/ListItem.tsx
@@ -1,4 +1,3 @@
-import { Component, ReactNode } from 'react';
 import { ICharacter } from '../../getCharacters';
 import styles from './style.module.css';
 
@@ -6,19 +5,17 @@ interface CharacterProps {
   character: ICharacter;
 }
 
-export default class ListItem extends Component<CharacterProps> {
-  render(): ReactNode {
-    const { character } = this.props;
-
-    return (
-      <div className={styles.wrapper}>
-        <h1 className={styles.title}>{character.name}</h1>
-        <p>Race: {character.race}</p>
-        <p>Gender: {character.gender}</p>
-        <p>Spouse: {character.spouse}</p>
-        <p>Birth: {character.birth}</p>
-        <p>Death: {character.death}</p>
-      </div>
-    );
-  }
+const ListItem: React.FC<CharacterProps> = ({ character }) => {
+  return (
+    <div className={styles.wrapper}>
+      <h1 className={styles.title}>{character.name}</h1>
+      <p>Race: {character.race}</p>
+      <p>Gender: {character.gender}</p>
+      <p>Spouse: {character.spouse}</p>
+      <p>Birth: {character.birth}</p>
+      <p>Death: {character.death}</p>
+    </div>
+  );
 }
+
+export default ListItem;
