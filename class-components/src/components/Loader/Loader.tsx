bf2f548e8dacb530f90a1eeed95cf0a27f1c9267
Yanina Lysukha <yanina.lysukha@gmail.com>
2024-07-11 12:16:17 +0200

    feat: add a functional loader component

--- class-components/src/components/Loader/Loader.tsx
+++ class-components/src/components/Loader/Loader.tsx
@@ -1,9 +1,8 @@
-import { Component, ReactNode } from 'react';
 import styles from './style.module.css';
 import loaderIcon from '../../assets/images/ring-inscription.png';
 
-export default class Loader extends Component {
-  render(): ReactNode {
-    return <img className={styles.loader} src={loaderIcon}></img>;
-  }
+export default function Loader() {
+  return (
+    <img className={styles.loader} src={loaderIcon}></img>
+  )
 }
