9b157de8de26354bf9e6b703462460e34008bad5
Yanina Lysukha <yanina.lysukha@gmail.com>
2024-07-12 11:53:00 +0200

    feat: add pagination to Container component

--- class-components/src/components/Container/Container.tsx
+++ class-components/src/components/Container/Container.tsx
@@ -3,11 +3,14 @@ import SearchBar from '../SearchBar/SearchBar';
 import ListView from '../ListView/ListView';
 import { getAllCharacters, getSearchedCharacters, ICharacter } from '../../getCharacters';
 import Loader from '../Loader/Loader';
-import Footer from '../Footer/Footer';
+import Pagination from '../Pagination/Pagination';
+
+const TOTAL_PAGES = 10;
 
 const Container: React.FC = () => {
   const [characters, setCharacters] = useState<ICharacter[]>([]);
   const [loader, setLoader] = useState<boolean>(false);
+  const [currentPage, setCurrentPage] = useState<number>(1);
 
   const updateSearchValueInLS = useCallback((value: string) => {
     localStorage.setItem('value', value);
@@ -34,14 +37,29 @@ const Container: React.FC = () => {
     handleCharacters(localStorage.getItem('value') ?? '');
   }, [handleCharacters]);
 
+  const handlePageChange = (page: number) => {
+    if (page >= 1 && page <= TOTAL_PAGES) {
+      setCurrentPage(page);
+    }
+  }
+
   return (
     <>
       <SearchBar
         onSearch={() => handleCharacters(localStorage.getItem('value') ?? '')}
         updateSearchValue={updateSearchValueInLS}
       ></SearchBar>
-      {loader ? <ListView characters={characters}></ListView> : <Loader></Loader>}
-      <Footer></Footer>
+      {loader ? (
+        <>
+          <ListView characters={characters}></ListView>
+          <Pagination
+            currentPage={currentPage}
+            totalPages={TOTAL_PAGES}
+            onPageChange={handlePageChange}
+            >
+          </Pagination>
+        </>
+        ) : <Loader></Loader>}
     </>
   );
 };
--- class-components/src/components/Footer/Footer.tsx
+++
@@ -1,7 +0,0 @@
-import styles from './style.module.css';
-
-const Footer = () => {
-  return <div className={styles.footer}></div>;
-};
-
-export default Footer;
