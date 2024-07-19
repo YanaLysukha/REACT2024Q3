efcfbe099bec3d49ec8338fa9e99c499514c8301
Yanina Lysukha <yanina.lysukha@gmail.com>
2024-07-19 16:20:11 +0200

    refactor: replace styles

--- class-components/src/main.tsx
+++ class-components/src/main.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './styles/index.css';
 import App from './App';
 import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
