e8c3ad730dabb7575f82f97955d42ae7b3d696f6
Yanina Lysukha <yanina.lysukha@gmail.com>
2024-07-26 14:53:58 +0200

    feat: change pagination routing

--- class-components/src/pages/MainPage/Container.tsx
+++ class-components/src/pages/MainPage/Container.tsx
@@ -4,8 +4,7 @@ import ListView from '../../components/ListView/ListView';
 import { getCharacterById, getCharacters, ICharacter } from '../../services/getCharacters';
 import Loader from '../../components/Loader/Loader';
 import Pagination from '../../components/Pagination/Pagination';
-import { useSearchParams } from 'react-router-dom';
-import Details from '../../components/Details/Details';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 import styles from './style.module.css';
 
 const TOTAL_PAGES = 10;
@@ -13,10 +12,10 @@ const TOTAL_PAGES = 10;
 const MainPage: React.FC = () => {
   const [characters, setCharacters] = useState<ICharacter[] | null>([]);
   const [loader, setLoader] = useState<boolean>(false);
-  const [searchParams, setSearchParams] = useSearchParams();
-  const currentPage = parseInt(searchParams.get('page') || '1', 10);
-  const currentDetailId = searchParams.get('detailId');
-  const [currentCharacter, setCurrentCharacter] = useState<ICharacter>({} as ICharacter);
+  // const [searchParams, setSearchParams] = useSearchParams();
+  const navigate = useNavigate();
+  const [currentPage, setCurrentPage] = useState(1);
+  const { pathname } = useLocation();
 
   const updateSearchValueInLS = (value: string) => {
     localStorage.setItem('value', value);
@@ -24,7 +23,7 @@ const MainPage: React.FC = () => {
 
   const handleCharacter = async (detailId: string) => {
     const character = await getCharacterById(detailId);
-    setCurrentCharacter(character);
+    console.log(character);
   };
 
   const handleCharacters = async (value: string = '') => {
@@ -42,26 +41,16 @@ const MainPage: React.FC = () => {
 
   useEffect(() => {
     handleCharacters(localStorage.getItem('value') ?? '');
-  }, [searchParams]);
-  // add current page later
+  }, [currentPage]);
 
   const handlePageChange = (page: number) => {
-    if (page >= 1 && page <= TOTAL_PAGES) {
-      if (currentDetailId !== null) {
-        setSearchParams({ page: page.toString(), detailId: currentDetailId });
-      } else {
-        setSearchParams({ page: page.toString() });
-      }
-    }
-  };
-
-  const handleCharacterDeletion = () => {
-    setCurrentCharacter({} as ICharacter);
+    setCurrentPage(page);
+    navigate(`${pathname}?page=${page}`);
   };
 
   return (
-    <div className={currentCharacter ? styles.split : ''}>
-      <div className={styles.left} onClick={handleCharacterDeletion}>
+    <div className={''}>
+      <div className={styles.left}>
         <SearchBar
           onSearch={() => handleCharacters(localStorage.getItem('value') ?? '')}
           updateSearchValue={updateSearchValueInLS}
@@ -79,11 +68,7 @@ const MainPage: React.FC = () => {
           <Loader></Loader>
         )}
       </div>
-      {Object.keys(currentCharacter).length > 0 && (
-        <div className={styles.right}>
-          <Details />
-        </div>
-      )}
+      <Outlet></Outlet>
     </div>
   );
 };
